23b277caa82b3f6a025f9952b7f5882c3b8a8658
Мухаматдинов И. Р <GBardeec@users.noreply.github.com>
2026-09-04 22:50:49 +0400

    Schedule vacancy import every three days

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('app:test')
+    ->cron('15 3 */3 * *')
+    ->timezone('Europe/Samara')
+    ->withoutOverlapping(180)
+    ->appendOutputTo(storage_path('logs/vacancy-import.log'));
